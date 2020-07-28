c45540bacdad1051186cbbc500be929ecba63292
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2020-07-28 12:34:56 +0530

    Updates

---
+++ precache-manifest.a2730c9aadbc90d560958c54ffad777d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2db27486ac26a801ae08bb5117eedb",
+    "url": "/Albums-React/index.html"
+  },
+  {
+    "revision": "79266e67b8f0b1852f90",
+    "url": "/Albums-React/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "79266e67b8f0b1852f90",
+    "url": "/Albums-React/static/js/2.5c22d0e8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Albums-React/static/js/2.5c22d0e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2467555214f0cfdbe7",
+    "url": "/Albums-React/static/js/main.318af8aa.chunk.js"
+  },
+  {
+    "revision": "09b834eed75b488e1890",
+    "url": "/Albums-React/static/js/runtime-main.1a5b3a6d.js"
+  }
+]);
